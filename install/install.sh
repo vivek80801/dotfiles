3979c5fcf2bfb7e3c04f6d76a7fa68703673f68f
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-07-12 11:29:46 +0530

    changing permissions of file

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 
 echo "linking all files"
 ln -s ./.gitconfig ~/.gitconfig
@@ -9,6 +9,13 @@ ln -s ./.Xresources ~/.Xresources
 ln -s ./nvim ~/.config/nvim
 ln -s ./awesome ~/.config/awesome
 echo "all files are linked"
+
+echo "Changing permission to all install scripts in this directory"
+chmod +x ./git.sh
+chmod +x ./vim.sh
+chmod +x ./nvim.sh
+chmod +x ./awesome.sh
+chmod +x ./brave.sh
 
 echo "Installing softwares"
 ./git.sh
