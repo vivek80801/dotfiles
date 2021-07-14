4205ec28b681dceca641bac96186a2ef4cdb53b6
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-07-14 13:35:58 +0530

    background change

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -22,10 +22,6 @@ require("awful.hotkeys_popup.keys")
 local debian = require("debian.menu")
 local has_fdo, freedesktop = pcall(require, "freedesktop")
 
--- My Wallpapers
-local myWallpapers = {"Spices_in_Athens_by_Makis_Chourdakis.jpg", "Raindrops_On_The_Table_by_Alex_Fazit.jpg", "Cielo_estrellado_by_Eduardo_Diez_Viñuela.jpg", "brad-huchteman-stone-mountain.jpg", "Focal-Fossa_WP_4096x2304_GREY.png", "warty-final-ubuntu.png"}
-local myRandom = math.floor(math.random() * 2)
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -183,7 +179,7 @@ local function set_wallpaper(s)
         -- default wallpaper
         -- gears.wallpaper.maximized(wallpaper, s, true)
         -- my wallpaper
-        gears.wallpaper.maximized("/usr/share/backgrounds/hardy_wallpaper_uhd.png", s, true)
+        gears.wallpaper.maximized("/usr/share/backgrounds/TCP118v1_by_Tiziano_Consonni.jpg", s, true)
     end
 end
 
