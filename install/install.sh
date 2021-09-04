1c39536447e5db7b1237c26d1b490d29e8298f23
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-09-04 07:04:56 +0530

    install script updated

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "linking all files"
-ln -s ./.gitconfig ~/.gitconfig
-ln -s ./.vimrc ~/.vimrc
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.zshrc ~/.zshrc
-ln -s ./.Xresources ~/.Xresources
-ln -s ./nvim ~/.config/nvim
-ln -s ./awesome ~/.config/awesome
+ln -s ../.gitconfig ~/.gitconfig
+ln -s ../.vimrc ~/.vimrc
+ln -s ../.tmux.conf ~/.tmux.conf
+ln -s ../.zshrc ~/.zshrc
+ln -s ../.Xresources ~/.Xresources
+ln -s ../nvim ~/.config/nvim
+ln -s ../awesome ~/.config/awesome
 echo "all files are linked"
 
 echo "Changing permission to all install scripts in this directory"
